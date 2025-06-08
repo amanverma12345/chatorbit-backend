c9ed8f9b36418a2b19b3de05b2580e437fe1a3a0
amanverma12345 <av7080181848@gmail.com>
2025-06-08 21:01:44 +0530

    socket

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -7,29 +7,35 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://chatorbit-frontend.vercel.app"],
   },
 });
+
+// used to store online users
+const userSocketMap = {}; // {userId: socketId}
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
-
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
-  const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
+  // ✅ safer parsing of query
+  const url = new URL(`http://localhost?${socket.handshake.url.split("?")[1] || ""}`);
+  const userId = url.searchParams.get("userId");
 
-  // io.emit() is used to send events to all the connected clients
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
+
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
+    if (userId) {
+      delete userSocketMap[userId];
+    }
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
