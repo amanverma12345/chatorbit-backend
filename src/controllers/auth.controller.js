e6781444aae37350541fa01b92a5cb55cb89fdb7
amanverma12345 <av7080181848@gmail.com>
2025-06-10 22:26:07 +0530

    cookie erreser

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -52,7 +52,7 @@ export const signup = async (req, res) => {
 export const logout = async (req, res) => {
 
     try {
-        res.cookie("jwt", "", {masAge:0});
+        res.cookie("jwt", "", {maxAge:0});
         res.status(200).json({ message: "Logged out successfully"});
     } catch (error) {
         console.log("Error in logout controller", error.message);
